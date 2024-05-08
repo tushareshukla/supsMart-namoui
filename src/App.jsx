8fed83421dc8c6d15bb10178ed4b1adf5d711cf7
bhudev <tushareshukla@gmail.com>
2024-05-08 09:55:16 +0530

    pages-folder-structure-defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { ToastBar } from "react-hot-toast";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Products from "./Pages/Products/Products";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Notfound from "./Pages/NotFound/Notfound";
+import SingleProduct from "./Pages/SingleProducts/SingleProduct";
 function App() {
   return (
     <div className="app">
-      <ToastBar position="top-right" />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/" element={<Products/>}/>
+      {/* <ToastBar position="top-right" /> */}
 
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singup" element={<Signup />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
